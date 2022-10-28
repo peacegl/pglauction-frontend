f5f11548bc9d6bfb4164e1112c9b0d306a3eb61d
samimhamza-pgl <samim.hamza@peacegl.com>
2022-10-28 17:07:51 +0430

    vehicle crud in progress

--- src/modules/admin/vehicles/VehicleModal.js
+++ src/modules/admin/vehicles/VehicleModal.js
@@ -1,37 +1,60 @@
+import DirectionsCarIcon from '@mui/icons-material/DirectionsCar';
+import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
+import CollectionsIcon from '@mui/icons-material/Collections';
+import VehicleConfigs from '../../../configs/pages/vehicles';
+import VehicleStepOne from './VehicleStepOne';
+import VehicleStepTwo from './VehicleStepTwo';
 import CustomModal from '../../CustomModal';
-import AddCircleIcon from '@mui/icons-material/AddCircle';
 import Box from '@mui/material/Box';
 import PropTypes from 'prop-types';
 
 const steps = [
   {
     key: 1,
-    icon: <AddCircleIcon />,
+    icon: <DirectionsCarIcon />,
     label: 'Vehicle Properties',
-    children: <Box>First Step</Box>,
+    children: <VehicleStepOne />,
   },
   {
     key: 2,
-    icon: <AddCircleIcon />,
-    label: 'Vehicle Info',
-    children: <Box>Second Step</Box>,
+    icon: <ShoppingBagIcon />,
+    label: 'Auction Details',
+    children: <VehicleStepTwo />,
   },
   {
-    key: 1,
-    icon: <AddCircleIcon />,
-    label: 'Auction Properties',
+    key: 3,
+    icon: <CollectionsIcon />,
+    label: 'Vehicle Images',
     children: <Box>Third Step</Box>,
   },
 ];
 
+const validationSchema = VehicleConfigs().validationSchema;
+
 export default function VehicleModal({open, toggleOpen, width, ...rest}) {
+  const onSave = (values) => {
+    console.log('fff', values);
+  };
+
   return (
     <CustomModal
       open={open}
       toggleOpen={toggleOpen}
       width={width}
-      {...rest}
       steps={steps}
+      onSave={onSave}
+      validationSchema={validationSchema}
+      initialValues={{
+        vin: '',
+        lot_number: '',
+        year: '',
+        model: '',
+        color: '',
+        engine_type: '',
+        cylinders: '',
+        vehicle_type: '',
+      }}
+      {...rest}
     />
   );
 }
