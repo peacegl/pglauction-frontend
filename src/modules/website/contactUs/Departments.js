1743812dcd716a194b78fd1ff4074d36207b38a6
samimhamza-pgl <samim.hamza@peacegl.com>
2022-12-05 16:04:11 +0430

    contents added

--- src/modules/website/contactUs/Departments.js
+++ src/modules/website/contactUs/Departments.js
@@ -16,19 +16,31 @@ const departments = [
       {
         icon: <EmailIcon sx={{color: (theme) => theme.palette.info.main}} />,
         content: 'dispatch@peacegl.com',
-        url: 'mailto:samim.hamza@peacegl.com',
+        url: 'mailto:dispatch@peacegl.com',
       },
       {
         icon: (
           <PhoneEnabledIcon sx={{color: (theme) => theme.palette.info.main}} />
         ),
-        content: '+1 (912) 228 8160',
-        url: 'tel:+93749740202',
+        content: '+1 (912) 228-8160',
+        url: 'tel:+19122288160',
+      },
+      {
+        icon: (
+          <PhoneEnabledIcon sx={{color: (theme) => theme.palette.info.main}} />
+        ),
+        content: '+1 (912) 239-5184',
+        url: 'tel:+19122395184',
       },
       {
         icon: <WhatsAppIcon sx={{color: (theme) => theme.palette.info.main}} />,
-        content: '+1 (912) 228 8160',
-        url: 'https://wa.me/+93749740202',
+        content: '+1 (912) 239-5182',
+        url: 'https://wa.me/+19122395182',
+      },
+      {
+        icon: <WhatsAppIcon sx={{color: (theme) => theme.palette.info.main}} />,
+        content: '+1 (912) 239-5184 ',
+        url: 'https://wa.me/+19122395184 ',
       },
       {
         icon: <PersonIcon sx={{color: (theme) => theme.palette.info.main}} />,
@@ -42,8 +54,8 @@ const departments = [
     details: [
       {
         icon: <EmailIcon sx={{color: (theme) => theme.palette.success.main}} />,
-        content: 'dispatch@peacegl.com',
-        url: 'mailto:samim.hamza@peacegl.com',
+        content: 'accounting@peacegl.com',
+        url: 'mailto:accounting@peacegl.com',
       },
       {
         icon: (
@@ -51,30 +63,21 @@ const departments = [
             sx={{color: (theme) => theme.palette.success.main}}
           />
         ),
-        content: '+1 (912) 228 8160',
-        url: 'tel:+93749740202',
-      },
-      {
-        icon: (
-          <PhoneEnabledIcon
-            sx={{color: (theme) => theme.palette.success.main}}
-          />
-        ),
-        content: '+1 (912) 228 8160',
-        url: 'tel:+93749740202',
+        content: '+1 (912) 376-1405',
+        url: 'tel:+19123761405',
       },
       {
         icon: (
           <WhatsAppIcon sx={{color: (theme) => theme.palette.success.main}} />
         ),
-        content: '+1 (912) 228 8160',
-        url: 'https://wa.me/+93749740202',
+        content: '+1 (912) 376-1405',
+        url: 'https://wa.me/+19123761405',
       },
       {
         icon: (
           <PersonIcon sx={{color: (theme) => theme.palette.success.main}} />
         ),
-        content: 'M. Hussain',
+        content: 'Samirullah',
       },
     ],
   },
@@ -84,22 +87,22 @@ const departments = [
     details: [
       {
         icon: <EmailIcon sx={{color: deepPurple[400]}} />,
-        content: 'dispatch@peacegl.com',
-        url: 'mailto:samim.hamza@peacegl.com',
+        content: 'title@peacegl.com',
+        url: 'mailto:title@peacegl.com',
       },
       {
         icon: <PhoneEnabledIcon sx={{color: deepPurple[400]}} />,
-        content: '+1 (912) 228 8160',
-        url: 'tel:+93749740202',
+        content: '+1 (912) 228-8160',
+        url: 'tel:+19122288160',
       },
       {
         icon: <WhatsAppIcon sx={{color: deepPurple[400]}} />,
-        content: '+1 (912) 228 8160',
-        url: 'https://wa.me/+93749740202',
+        content: '+1 (912) 376-0020',
+        url: 'https://wa.me/+19123760020',
       },
       {
         icon: <PersonIcon sx={{color: deepPurple[400]}} />,
-        content: 'M. Hussain',
+        content: 'M. Faisal',
       },
     ],
   },
@@ -111,8 +114,8 @@ const departments = [
         icon: (
           <EmailIcon sx={{color: (theme) => theme.palette.secondary.dark}} />
         ),
-        content: 'dispatch@peacegl.com',
-        url: 'mailto:samim.hamza@peacegl.com',
+        content: 'docs@peacegl.com',
+        url: 'mailto:docs@peacegl.com',
       },
       {
         icon: (
@@ -120,21 +123,28 @@ const departments = [
             sx={{color: (theme) => theme.palette.secondary.dark}}
           />
         ),
-        content: '+1 (912) 228 8160',
-        url: 'tel:+93749740202',
+        content: '+1 (912) 239-5160',
+        url: 'tel:+19122395160',
       },
       {
         icon: (
           <WhatsAppIcon sx={{color: (theme) => theme.palette.secondary.dark}} />
         ),
-        content: '+1 (912) 228 8160',
-        url: 'https://wa.me/+93749740202',
+        content: '+1 (912) 376-1024',
+        url: 'https://wa.me/+19123761024',
+      },
+      {
+        icon: (
+          <WhatsAppIcon sx={{color: (theme) => theme.palette.secondary.dark}} />
+        ),
+        content: '+1 (912) 376-1021',
+        url: 'https://wa.me/+19123761021',
       },
       {
         icon: (
           <PersonIcon sx={{color: (theme) => theme.palette.secondary.dark}} />
         ),
-        content: 'M. Hussain',
+        content: 'M. Haris',
       },
     ],
   },
