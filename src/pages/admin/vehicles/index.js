ae72143ecfdc0496f6797e5374d6c825d02c7992
samimhamza-pgl <samim.hamza@peacegl.com>
2022-12-07 09:54:35 +0430

    changes

--- src/pages/admin/vehicles/index.js
+++ src/pages/admin/vehicles/index.js
@@ -7,59 +7,59 @@ const Vehicles = asyncComponent(() =>
 );
 export default AppPage(() => <Vehicles />);
 
-import {UseServerSideProps} from '../../../customHooks';
-import jwtAxios from '@crema/services/auth/jwt-auth';
-export async function getServerSideProps(context) {
-  // try {
-  const res = await jwtAxios.get(`/auth`);
-  console.log('dsfasdfd', res.data);
-  if (res.status === 200) {
-    if (data.permissions?.includes('view_vehicles')) {
-      return {props: {}};
-    }
-  }
-  return {
-    redirect: {
-      destination: '/admin',
-      permanent: false,
-    },
-  };
+// import {UseServerSideProps} from '../../../customHooks';
+// import jwtAxios from '@crema/services/auth/jwt-auth';
+// export async function getServerSideProps(context) {
+//   // try {
+//   const res = await jwtAxios.get(`/auth`);
+//   console.log('dsfasdfd', res.data);
+//   if (res.status === 200) {
+//     if (data.permissions?.includes('view_vehicles')) {
+//       return {props: {}};
+//     }
+//   }
+//   return {
+//     redirect: {
+//       destination: '/admin',
+//       permanent: false,
+//     },
+//   };
 
-  // } catch (error) {
-  //   return {
-  //     redirect: {
-  //       destination: '/admin',
-  //       permanent: false,
-  //     },
-  //   };
-  // }
-  jwtAxios
-    .get('/auth')
-    .then(({data}) => {
-      console.log('dsfasdfd', data);
-      if (data.permissions?.includes('view_vehicles')) {
-        return {props: {}};
-      }
-      return {
-        redirect: {
-          destination: '/admin',
-          permanent: false,
-        },
-      };
-    })
-    .catch(() => {
-      return {
-        redirect: {
-          destination: '/admin',
-          permanent: false,
-        },
-      };
-    });
-  return {
-    redirect: {
-      destination: '/admin',
-      permanent: false,
-    },
-  };
-  // return UseServerSideProps('view_vehicles');
-}
+//   // } catch (error) {
+//   //   return {
+//   //     redirect: {
+//   //       destination: '/admin',
+//   //       permanent: false,
+//   //     },
+//   //   };
+//   // }
+//   jwtAxios
+//     .get('/auth')
+//     .then(({data}) => {
+//       console.log('dsfasdfd', data);
+//       if (data.permissions?.includes('view_vehicles')) {
+//         return {props: {}};
+//       }
+//       return {
+//         redirect: {
+//           destination: '/admin',
+//           permanent: false,
+//         },
+//       };
+//     })
+//     .catch(() => {
+//       return {
+//         redirect: {
+//           destination: '/admin',
+//           permanent: false,
+//         },
+//       };
+//     });
+//   return {
+//     redirect: {
+//       destination: '/admin',
+//       permanent: false,
+//     },
+//   };
+//   // return UseServerSideProps('view_vehicles');
+// }
