f4b22fda36289340028d58dd228fbdfc7d6ed7aa
azizpgl <111742707+azizpgl@users.noreply.github.com>
2022-12-14 00:59:48 +0430

    default image set for vehicel in website

--- src/modules/website/vehicles/VehicleList/index.js
+++ src/modules/website/vehicles/VehicleList/index.js
@@ -30,7 +30,7 @@ const VehicleList = () => {
   useEffect(() => {
     dispatch(
       onGetWebVehicleData({
-        filterData,
+        // filterData,
         per_page: perPage,
         page: page + 1,
         search,
