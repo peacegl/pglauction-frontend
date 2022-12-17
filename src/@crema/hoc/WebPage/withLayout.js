64b60a0b68af11688c5837fabca404c71fb280a9
samimhamza-pgl <samim.hamza@peacegl.com>
2022-12-17 12:09:32 +0430

    pagination added on vehicles

--- src/@crema/hoc/WebPage/withLayout.js
+++ src/@crema/hoc/WebPage/withLayout.js
@@ -1,11 +1,32 @@
-import AppBar from '../../../components/layoutComponents/AppBar';
-import AppThemeSetting from '../../core/AppThemeSetting';
-import Container from '@mui/material/Container';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import AppBar from 'components/layoutComponents/AppBar';
 import Header from 'components/layoutComponents/Header';
 import Footer from 'components/layoutComponents/Footer';
 import AppInfoView from '@crema/core/AppInfoView';
+import {useEffect, useState} from 'react';
+import {Fab, Tooltip} from '@mui/material';
+import IntlMessages from '@crema/utility/IntlMessages';
 
 const withLayout = (ComposedComponent) => (props) => {
+  const [visible, setVisible] = useState(false);
+  const toggleVisible = () => {
+    const scrolled = document.documentElement.scrollTop;
+    if (scrolled > 200) {
+      setVisible(true);
+    } else if (scrolled <= 200) {
+      setVisible(false);
+    }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+  useEffect(() => {
+    window.addEventListener('scroll', toggleVisible);
+  }, []);
   return (
     <div>
       <Header />
@@ -13,6 +34,22 @@ const withLayout = (ComposedComponent) => (props) => {
       <ComposedComponent {...props} />
       <AppInfoView />
       <Footer />
+      <Tooltip title={<IntlMessages id='common.scrollToTop' />} placement='top'>
+        <Fab
+          onClick={scrollToTop}
+          size='small'
+          color='primary'
+          sx={{
+            display: visible ? 'flex' : 'none',
+            position: 'fixed',
+            bottom: '20px',
+            right: '50px',
+            zIndex: 99,
+          }}
+        >
+          <KeyboardArrowUpIcon />
+        </Fab>
+      </Tooltip>
     </div>
   );
 };
