dd3522ef441544048ef8e4425f12b54bc3c13671
azizpgl <111742707+azizpgl@users.noreply.github.com>
2022-12-19 20:20:22 +0430

    dashboard count of vehicles

--- src/configs/index.js
+++ src/configs/index.js
@@ -58,6 +58,16 @@ export function moneyFormater(number, currency = 'AED') {
   return formater.format(number);
 }
 
+// Create our number formatter.
+export function numberFormater(number, decimal = 0) {
+  const formater = new Intl.NumberFormat('en-US', {
+    // These options are needed to round to whole numbers if that's what you want.
+    //minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
+    //maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
+  });
+  return formater.format(number, decimal);
+}
+
 export function range(start, end, step) {
   return Array.from(
     Array.from(Array(Math.ceil((end - start) / step)).keys()),
---
+++ src/modules/admin/dashboard/index.js
@@ -0,0 +1,83 @@
+import {AppAnimate, AppGridContainer} from '@crema';
+import {Box, Grid} from '@mui/material';
+import CardState from 'components/design/CardState';
+import {useEffect} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import {getAdminCounts} from 'redux/actions';
+
+export default function Dashboard() {
+  const dispatch = useDispatch();
+  const {vehicles = {}} = useSelector(({common}) => common.counts);
+  useEffect(() => {
+    dispatch(getAdminCounts());
+  }, []);
+
+  return (
+    <>
+      <AppAnimate animation='transition.slideUpIn' delay={200}>
+        <Box
+          sx={{
+            display: 'flex',
+            // alignItems: 'center',
+            columnGap: 5,
+            rowGap: 3,
+            flexWrap: 'wrap',
+
+            pb: {xs: 5, md: 8},
+          }}
+        >
+          <Box component='span' sx={{flex: '1 1 15%'}}>
+            <CardState
+              item={{
+                title: 'All Vehicles',
+                count: vehicles.all,
+                color: '#9E49E6',
+                icon: 'A',
+              }}
+            />
+          </Box>
+          <Box component='span' sx={{flex: '1 1 15%'}}>
+            <CardState
+              item={{
+                title: 'Future Vehicles',
+                count: vehicles.future,
+                color: '#ff9800',
+                icon: 'F',
+              }}
+            />
+          </Box>
+          <Box component='span' sx={{flex: '1 1 15%'}}>
+            <CardState
+              item={{
+                title: 'Available Vehicle',
+                count: vehicles.available,
+                color: '#4caf50',
+                icon: 'AV',
+              }}
+            />
+          </Box>
+          <Box component='span' sx={{flex: '1 1 15%'}}>
+            <CardState
+              item={{
+                title: 'Sold Vehicles',
+                count: vehicles.sold,
+                color: '#b23c17',
+                icon: 'S',
+              }}
+            />
+          </Box>
+          <Box component='span' sx={{flex: '1 1 15%'}}>
+            <CardState
+              item={{
+                title: 'Inactive Vehicles',
+                count: vehicles.inactive,
+                color: '#009688',
+                icon: 'IN',
+              }}
+            />
+          </Box>
+        </Box>
+      </AppAnimate>
+    </>
+  );
+}
