a36039558df4e7d3452a35c2a535f30d9c0db10e
nasim-alizai-pgl <nasim@peaceglobaltech.com>
2023-02-12 20:55:21 +0430

    add some design in bid info

--- src/modules/website/auctions/AuctionItems/BidInfo.js
+++ src/modules/website/auctions/AuctionItems/BidInfo.js
@@ -6,13 +6,34 @@ import PropTypes from 'prop-types';
 import {
   alpha,
   Box,
+  Button,
   Card,
   CardContent,
   CardHeader,
+  Chip,
+  InputAdornment,
+  TextField,
   Typography,
 } from '@mui/material';
 
 export default function BidInfo({vehicle}) {
+  const [bidValue, setBidValue] = useState('');
+  const [bidError, setBidError] = useState(false);
+
+  const buyNow = () => {
+    //code here buy now price
+    console.log(vehicle);
+  };
+
+  const bidChange = (e) => {
+    setBidValue(e.target.value);
+    if (parseFloat(e.target.value) < vehicle?.minimum_bid) {
+      setBidError(true);
+    } else {
+      setBidError(false);
+    }
+  };
+
   return (
     <Card sx={{borderRadius: 1, boxShadow: 1, m: 0, minHeight: 400}}>
       <CardHeader
@@ -32,23 +53,88 @@ export default function BidInfo({vehicle}) {
           </Typography>
         }
       />
-      <CardContent sx={{px: 3, py: 0}}>
-        <Item
-          label={<IntlMessages id='bid.bidStatus' />}
-          value={
-            vehicle.bid_status == 1
-              ? 'You have set the highest bid'
-              : vehicle.bid_status == 2
-              ? 'Your bid is lower than the current bid'
-              : "You Haven't Bid"
-          }
-        />
-        <Item
-          label={<IntlMessages id='bid.currentBid' />}
-          value={moneyFormater(
-            vehicle.bids[0]?.amount ? vehicle.bids[0]?.amount : 0,
-          )}
-        />
+      <CardContent
+        sx={{
+          px: 3,
+          py: 0,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Box>
+          <Item
+            label={<IntlMessages id='bid.bidStatus' />}
+            value={
+              vehicle.bid_status == 1
+                ? 'You have set the highest bid'
+                : vehicle.bid_status == 2
+                ? 'Your bid is lower than the current bid'
+                : "You Haven't Bid"
+            }
+          />
+          <Item
+            label={<IntlMessages id='bid.minimum_bid' />}
+            value={moneyFormater(vehicle?.minimum_bid)}
+          />
+
+          <Item
+            label={<IntlMessages id='bid.currentBid' />}
+            value={moneyFormater(
+              vehicle.bids[0]?.amount ? vehicle.bids[0]?.amount : 0,
+            )}
+          />
+
+          <Item
+            label={<IntlMessages id='common.buy_now_price' />}
+            value={
+              <Chip
+                sx={{
+                  px: 2,
+                  textTransform: 'capitalize',
+                  fontWeight: 'bold',
+                  color: (theme) => theme.palette.primary.contrastText,
+                  bgcolor: '#ffa834',
+                  '&:hover': {
+                    backgroundColor: '#c98709',
+                  },
+                }}
+                label={moneyFormater(vehicle?.buy_now_price)}
+                size='small'
+                onClick={() => buyNow()}
+              />
+            }
+          />
+
+          <TextField
+            sx={{
+              mt: 12,
+            }}
+            fullWidth
+            label={'Enter your bid value'}
+            value={bidValue}
+            error={bidError}
+            helperText={bidError ? 'Incorrect entry.' : ''}
+            onChange={(e) => bidChange(e)}
+            size='small'
+            type='number'
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position='start'>
+                  <IntlMessages id='common.AED' />
+                </InputAdornment>
+              ),
+            }}
+          />
+        </Box>
+
+        <Button
+          variant='contained'
+          sx={{mt: 2, px: 6, borderRadius: 1, width: '50%'}}
+          onClick={() => console.log('bid')}
+        >
+          <IntlMessages id='bid.bid' />
+        </Button>
       </CardContent>
     </Card>
   );
