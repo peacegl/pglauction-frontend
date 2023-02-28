dd45762408bf8f012865f48d84821c9cf91bd898
nasim-alizai-pgl <nasim@peaceglobaltech.com>
2023-02-28 17:46:08 +0430

    changes

--- src/modules/admin/auctions/vehicleList/DealsTable/index.js
+++ src/modules/admin/auctions/vehicleList/DealsTable/index.js
@@ -3,12 +3,40 @@ import Table from '@mui/material/Table';
 import TableHead from '@mui/material/TableHead';
 import TableBody from '@mui/material/TableBody';
 import PropTypes from 'prop-types';
-import TableHeading from './TableHeading';
+
 import TableItem from './TableItem';
 import AppTableContainer from '../../../../../@crema/core/AppTableContainer';
+import TableHeading from 'components/CustomTableHeading/TableHeading';
 
 const DealsTable = (props) => {
   const {dealsTableData} = props;
+
+  const header = [
+    {id: 'common.code'},
+    {id: 'common.image', align: 'left'},
+    {id: 'common.vin', align: 'left'},
+    {id: 'common.lot_number', align: 'left'},
+    {id: 'common.minimum_bid', align: 'left'},
+    {id: 'common.buy_now_price', align: 'left'},
+    {id: 'common.bid_status', align: 'left'},
+    {id: 'common.totalCost', align: 'left'},
+    {id: 'common.saleRate', align: 'left'},
+    {id: 'common.price', align: 'left'},
+    {id: 'common.year', align: 'left'},
+    {id: 'common.make', align: 'left'},
+    {id: 'sidebar.mui.util.modal', align: 'left'},
+    {id: 'common.status', align: 'left'},
+    {id: 'vehicle.exterior_color', align: 'left'},
+    {id: 'vehicle.interior_color', align: 'left'},
+    {id: 'vehicle.primary_damage', align: 'left'},
+    {id: 'vehicle.is_featured', align: 'left'},
+    {id: 'vehicle.is_best_selling', align: 'left'},
+    {id: 'common.engine_type', align: 'left'},
+    {id: 'vehicle.odometer', align: 'left'},
+    {id: 'vehicle.transmission', align: 'left'},
+    {id: 'common.created_by', align: 'left'},
+    {id: 'common.updated_by', align: 'left'},
+  ];
 
   return (
     <AppTableContainer sxStyle={{height: '500px'}}>
@@ -18,7 +46,7 @@ const DealsTable = (props) => {
             borderBottom: '0 none',
           }}
         >
-          <TableHeading />
+          <TableHeading header={header} />
         </TableHead>
         <TableBody
           sx={{
--- src/modules/admin/roles/user_roles/user_roles_tables/index.js
+++ src/modules/admin/roles/user_roles/user_roles_tables/index.js
@@ -3,11 +3,74 @@ import Table from '@mui/material/Table';
 import TableHead from '@mui/material/TableHead';
 import TableBody from '@mui/material/TableBody';
 import PropTypes from 'prop-types';
-import TableHeading from './TableHeading';
 import TableItem from './TableItem';
 import AppTableContainer from '../../../../../@crema/core/AppTableContainer';
+import TableHeading from 'components/CustomTableHeading/TableHeading';
 
 const TicketSupportTable = ({ticketSupportData}) => {
+  const header = [
+    {
+      id: 'common.profile',
+    },
+    {
+      id: 'common.code',
+      algin: 'left',
+    },
+    {
+      id: 'common.username',
+      algin: 'left',
+    },
+    {
+      id: 'common.fullname',
+      algin: 'left',
+    },
+    {
+      id: 'common.phone',
+      algin: 'left',
+    },
+    {
+      id: 'common.whatsapp',
+      algin: 'left',
+    },
+    {
+      id: 'common.gender',
+      algin: 'left',
+    },
+    {
+      id: 'common.email',
+      algin: 'left',
+    },
+    {
+      id: 'common.status',
+      algin: 'left',
+    },
+    {
+      id: 'common.type',
+      algin: 'left',
+    },
+    {
+      id: 'common.birthDate',
+      algin: 'left',
+    },
+    {
+      id: 'common.created_by',
+      algin: 'left',
+    },
+
+    {
+      id: 'common.created_at',
+      algin: 'left',
+    },
+    {
+      id: 'common.updated_by',
+      algin: 'left',
+    },
+    {
+      id: 'common.updated_at',
+      algin: 'left',
+    },
+  ];
+
   return (
     <AppTableContainer sxStyle={{height: '500px'}}>
       <Table>
@@ -16,7 +79,7 @@ const TicketSupportTable = ({ticketSupportData}) => {
             borderBottom: '0 none',
           }}
         >
-          <TableHeading />
+          <TableHeading header={header} />
         </TableHead>
         <TableBody
           sx={{
