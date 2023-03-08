3821714f6a1965fb36fcd119636f657dfc804734
nasim-alizai-pgl <nasim@peaceglobaltech.com>
2023-03-08 10:34:33 +0430

    changes

--- src/pages/live-auctions/[id].js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import AppPage from '../../@crema/hoc/WebPage';
-import asyncComponent from '../../@crema/utility/asyncComponent';
-
-const AuctionDetail = asyncComponent(() =>
-  import('../../modules/auction_items/AuctionDetail'),
-);
-export default AppPage(() => <AuctionDetail />);
