e3c7cdcb0f679894f9738f0d814f3049d29c28b5
HajiRohullah <rohullahhussaini67@gmail.com>
2023-10-28 22:02:28 +0430

    save changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import AppPage from '../@crema/hoc/WebPage';
 import asyncComponent from '../@crema/utility/asyncComponent';
 
-const Home = asyncComponent(() => import('../modules/website/auction-software/index.js'));
-// const Home = asyncComponent(() => import('../modules/website/home/index'));
+// const Home = asyncComponent(() => import('../modules/website/auction-software/index.js'));
+const Home = asyncComponent(() => import('../modules/website/home/index'));
 export default AppPage(() => <Home />);
